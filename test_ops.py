21ba7756e3b998f672bde547f902ca66cb748eab
Syed Abrar Ahmed <abrar.syed860@gmail.com>
2025-06-29 21:24:10 -0400

    Added ops and test_ops

---
+++ test_ops.py
@@ -0,0 +1,13 @@
+from ops import *
+
+def test_add():
+    assert add(2,3)==5
+
+def test_subtract():
+    assert subtract(5,3)==2
+
+def test_multiply():
+    assert multiply(3,5)==15
+
+def test_divide():
+    assert divide(10,2)==5
